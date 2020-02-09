c222779e5c7986e9e5e88121380dcf6e0202a8fd
Williams <ciberwswilli@gmail.com>
2020-02-09 14:17:45 -0600

    agregar archivo de tarea 3

---
+++ Variables y estructuras de control.js
@@ -0,0 +1,20 @@
+var mes=10
+var carnet=" Carnet: 20003935"
+var nombre="Nombre: Williams Fernando De Leon"
+
+if(mes <= 2){
+alert("es invierno");
+}
+else if(mes ==12){
+alert("es invierno");}
+else if(mes <= 5){
+alert("es primavera");
+}
+else if(mes <=8){
+alert("es verano");
+}
+else if(mes <= 11){
+alert("es otoño");
+}
+
+alert( carnet+" " +" "+ nombre )
